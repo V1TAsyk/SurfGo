fd11ddb5f67280d2676a13e24d6e265081894a39
V1TAsyk <vito192917@gmail.com>
2025-02-18 16:42:35 +0100

    Create travel.js

---
+++ travel.js
@@ -0,0 +1,27 @@
+document.getElementById("scroll-travel").addEventListener("click", function () {
+  window.scrollTo({
+    top: 3420,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("book-flight").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("book-flight-p").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("travel-svg").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});

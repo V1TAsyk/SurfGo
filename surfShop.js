7b53095d56d942ec5f166039d59af9135e9d2104
V1TAsyk <vito192917@gmail.com>
2025-02-18 16:42:13 +0100

    Create surfShop.js

---
+++ surfShop.js
@@ -0,0 +1,56 @@
+document.getElementById("scroll-shop").addEventListener("click", () => {
+  window.scrollTo({
+    top: 7200,
+    behavior: "smooth",
+  });
+});
+
+document
+  .getElementById("book-flight-shop")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+document
+  .getElementById("book-flight-shoP")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+document.getElementById("shop-svg").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+document
+  .getElementById("book-flight-FFF")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+document
+  .getElementById("book-flight-fff")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+document.getElementById("footer-svg").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
